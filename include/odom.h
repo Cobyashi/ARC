ffa2edfb25b0ba66fa6bd645db3290033f6edbfa
DyeJoseph <josephpdye@icloud.com>
2024-10-13 02:14:53 -0500

    Basic setup of Odom

--- include/odom.h
+++ include/odom.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include "vex.h"
+
+using namespace vex;
+
+class Odom
+{
+    private:
+    float x_pos, y_pos;
+
+    public:
+
+    float get_current_X_position(float x_pos);
+    float get_current_Y_position(float y_pos);
+    void set_position(float x_pos, float y_pos);
+
+};
--- src/Odom.cpp
+++ src/Odom.cpp
@@ -1,1 +1,13 @@
 #include "Odom.h"
+
+float Odom::get_current_X_position(float x_pos){
+
+}
+
+float Odom::get_current_Y_position(float y_pos){
+
+}
+
+void Odom::set_position(float x_pos, float y_pos){
+
+}
